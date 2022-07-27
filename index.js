86a2a65c7792d8a61199d29aa09d46839401688f
Robert Bishop <rbishop85@gmail.com>
2022-07-26 21:03:20 -0700

    connected to required packages and files

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data)/ {}
 
 // TODO: Create a function to initialize app
 function init() {}
