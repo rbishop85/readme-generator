1caca89b013dbfb48979d301af0f45bd16e0fff3
Robert Bishop <rbishop85@gmail.com>
2022-07-26 22:03:47 -0700

    More work on questions, added some list functionality as well as some questions that only show up based on a previous question answer

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const questions = [
         type: 'input',
         message: 'What is your project title?',
         name: 'title',
+        prefix: 'Y',
     },
     {
         type: 'input',
@@ -19,6 +20,43 @@ const questions = [
         type: 'input',
         message: 'What is the name/s of the project creator/s?',
         name: 'creators',
+    },
+    {
+        type: 'input',
+        message: 'What steps are required to install your project?',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'What are the usage instructions for running your project?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Would You like to include an image?',
+        name: 'imageTrue',
+        choices: ["Yes", "No"]
+    },
+    {
+        type: 'input',
+        message: 'Enter the path to the image you wish to use.',
+        name: 'imagePath',
+        when: function(answers) {
+            return answers.imageTrue === "Yes";
+        }
+    },
+    {
+        type: 'input',
+        message: 'Enter descriptive alt text for the image.',
+        name: 'imageText',
+        when: function(answers) {
+            return answers.imageTrue === "Yes";
+        }
+    },
+    {
+        type: 'input',
+        message: 'This is just a test!',
+        name: 'test',
     },
 ];
 
